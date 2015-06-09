52da0220b90391683e2cf79827638c1864099f03
Rob1NN <lordrob1nn@googlemail.com>
2015-06-09 22:13:14 +0200

    most of the wörk is done.... well maybe not

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,22 @@
 class MainController < ApplicationController
   def index
-    if params[:id].nil?
-      new_id = rand(0..Flash.count)
-      redirect_to id: new_id
+  	id_not_zero = true
+  	if params[:id] == 0
+  		id_not_zero = false
+  	end
+  	id = params[:id].to_i
+    if id == 0 and id_not_zero
+    	redirect and return
+    elsif id.nil?
+      redirect and return
+    elsif id < 0
+      redirect and return
     else
       @flash = Flash.find(params[:id])
     end
+  end
+  def redirect
+    new_id = rand(0..Flash.count)
+    redirect_to(:id => new_id)
   end
 end
