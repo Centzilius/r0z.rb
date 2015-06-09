03c6e4b750fcf73034a2eea392d48aa53f1c15ed
Rob1NN <lordrob1nn@googlemail.com>
2015-06-10 00:02:40 +0200

    (not working) more progress!

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,11 +2,11 @@ class MainController < ApplicationController
   def index
   	id_not_zero = true
   	if params[:id] == "0"
-  		id_not_zero = false
+  	  id_not_zero = false
   	end
   	id = params[:id].to_i
     if id == 0 and id_not_zero
-    	redirect and return
+      redirect and return
     elsif id.nil?
       redirect and return
     elsif id < 0
@@ -19,4 +19,11 @@ class MainController < ApplicationController
     new_id = rand(0..Flash.count)
     redirect_to(:id => new_id)
   end
+  def flashratio
+  	data = FastImage.size("http://sandpapr.pw/flash/"+@flash.id.to_s+".swf")
+  	byebug
+  	ratio = data[0]/data[1]
+  	return ratio
+  end
+  helper_method :flashratio
 end
