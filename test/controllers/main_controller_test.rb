e11715bea3058a6417c16fa114559e934f0a6570
Rob1NN <lordrob1nn@googlemail.com>
2015-06-10 05:42:59 +0200

    fix test

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class MainControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_response :success
+    assert_response :redirect
   end
 
 end
